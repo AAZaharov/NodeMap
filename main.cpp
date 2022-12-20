8af5b58b2e22ce7cd16fe08d9c8dbfd50da61115
Artem <zaharov@localnet.tesis.com.ru>
2022-12-20 18:28:46 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <QtGui>
+#include <QApplication>
+
+#include "graphwidget.h"
+
+
+int main(int argc, char** argv)
+{
+
+   QApplication app(argc, argv);
+   qsrand(QTime(0, 0, 0).secsTo(QTime::currentTime()));
+
+   GraphWidget widget;
+   widget.show();
+   return app.exec();
+}
